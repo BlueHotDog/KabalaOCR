c5e315f0512d88997de66dd73aa4da85a0c67914
BlueHotDog <dannifriedland@gmail.com>
2011-12-03 02:06:26 +0200

    some of the first receipt is ready

---
+++ create_box_file.js
@@ -0,0 +1,11 @@
+var util = require('util'),
+	exec = require('child_process').exec,
+	child;
+// executes tesseract [lang].[fontname].exp[num].tif [lang].[fontname].exp[num] batch.nochop makebox
+child = exec("tesseract "+process.cwd()+"/"+process.argv[2]+".tiff "+process.cwd()+"/"+process.argv[2]+" batch.nochop makebox", function (error, stdout, stderr) {
+  util.print('stdout: ' + stdout);
+  util.print('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
